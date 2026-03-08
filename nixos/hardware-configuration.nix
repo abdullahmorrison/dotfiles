0c0365142c9f9d537eaefb6a0ded41d46586eb20
Abdullah Morrison <abdullahmorrison@gmail.com>
2026-03-07 17:40:42 -0800

    removed unnecessary hardware config

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -30,7 +30,6 @@
   hardware = {
     cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     graphics.enable = true;
-    nvidia.modesetting.enable = true;
   };
 }
 
